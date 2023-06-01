03b7d8cd2ca687ab434f1c96d49fe151779bb7c5
lookingspiffy <anthony.stu+github@gmail.com>
2023-06-01 10:34:19 -0700

    Updated folder tree.

--- vsphere/modules/vm_lab_folders/main.tf
+++ vsphere/modules/vm_lab_folders/main.tf
@@ -18,7 +18,7 @@ resource "vsphere_folder" "kubernetes" {
 }
 
 resource "vsphere_folder" "ansibletest" {
-  path          = "${vsphere_folder.parent.path}/AnsibleTest"
+  path          = "${vsphere_folder.parent.path}/Ansible"
   type          = "vm"
   datacenter_id = data.vsphere_datacenter.dc.id
 }
@@ -37,20 +37,14 @@ resource "vsphere_folder" "linux" {
 
 # Grandchild Folder(s)
 
-resource "vsphere_folder" "kubemanager" {
-  path          = "${vsphere_folder.kubernetes.path}/KubeManager"
-  type          = "vm"
-  datacenter_id = data.vsphere_datacenter.dc.id
-}
-
 resource "vsphere_folder" "kubemasters" {
-  path          = "${vsphere_folder.kubernetes.path}/KubeMasters"
+  path          = "${vsphere_folder.kubernetes.path}/Controllers"
   type          = "vm"
   datacenter_id = data.vsphere_datacenter.dc.id
 }
 
 resource "vsphere_folder" "kubeworkers" {
-  path          = "${vsphere_folder.kubernetes.path}/KubeWorkers"
+  path          = "${vsphere_folder.kubernetes.path}/Workers"
   type          = "vm"
   datacenter_id = data.vsphere_datacenter.dc.id
 }
