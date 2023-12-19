994c37a75fe1a792c1dface4110e498b9562c9bc
Looking Spiffy <106634781+lookingspiffy@users.noreply.github.com>
2023-12-19 13:55:18 -0800

    Update create-inventory-file.tf
    Adding module calling block.

--- ansible/create-inventory-file.tf
+++ ansible/create-inventory-file.tf
@@ -1,3 +1,17 @@
+# Relevant blocks for ansible inventory file creation.
+# For the full module, please see https://github.com/lookingspiffy/terraform/tree/main/vsphere/modules/vm_deploy_environment
+
+module "deploy_vms" {
+  source             = "./modules/vm_deploy_environment"
+  virtual_machines   = var.virtual_machines
+  vsphere_datacenter = var.vsphere_datacenter
+  vsphere_datastore  = var.vsphere_datastore
+  vsphere_host       = var.vsphere_host
+  vsphere_network    = var.vsphere_network
+
+  depends_on = [module.vm_lab_folders]
+}
+
 resource "local_file" "ansible_inventory" {
   content = templatefile("./templates/inventory.tpl", {
     user      = var.ansible_user
